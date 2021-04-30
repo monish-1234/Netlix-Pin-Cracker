9b479d518d79a4f76ef3ddab0fb7cd6393732a7a
Monish <jmonishram@gmail.com>
2021-04-30 22:05:32 +0530

    Range Fix

--- start.py
+++ start.py
@@ -3,10 +3,21 @@ import time
 
 
 
+
+print ("--------------------DISCLAIMER--------------------DISCLAIMER--------------------DISCLAIMER--------------------\n \n \n \t This Script does not stop automatically untill the entire script is finished so , you cannot use your computer untill the script is finished. \n \n Press Ctrl+C / Ctrl+Z in the shell to stop the script. \n \n Do not indulge in any illegal activities using this script \n \n The Developer IS NOT RESPONSIBLE for whatever way you choose to use this \n \n The script will start running in .... \n 14")
+time.sleep(2)#Delay to select the output window.
+print (" \n 12....")
 time.sleep(2)
+print (" \n 10....")
+time.sleep(2)
+print (" \n 8....")
+time.sleep(1)
+print (" \n 7....")
+time.sleep(7) 
+
 keyboard=Controller()
 
-for x in range(0000,2000):
+for x in range(0000,9999):
     pin=str(x)
     pin=pin.zfill(4)
     pin=list(pin)
